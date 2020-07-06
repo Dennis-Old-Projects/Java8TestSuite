a02d0b80cc710213926ac4df0b9d4e353429f1af
Dennis Chacko <dennis.x.chacko@gmail.com>
2020-07-06 12:19:37 -0500

    updated

--- src/test/java/com/cloudxpert/StreamsTestCase.java
+++ src/test/java/com/cloudxpert/StreamsTestCase.java
@@ -5,34 +5,79 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class StreamsTestCase {
 
-	private List<Employee> eList= null;
+	private static List<Employee> eList= null;
 	
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
 		Employee e1 = new Employee("Phani","Rayadu",1,100.0,"Mumbai");
-		Employee e2 = new Employee("Bill","Budd",2,100.0,"Mumbai");
+		Employee e2 = new Employee("Bill","Budd",2,200.0,"Mumbai");
 		Employee e3 = new Employee("Vamsi","Rao",3,100.0,"Surat");
 		Employee e4 = new Employee("Sowba","Madhavan",4,100.0,"Surat");
-		eList = new ArrayList<Employee>(Arrays.asList(e1,e2,e3,e4));
+		Employee e5 = new Employee("John","Lennon",5,300.0,"Mumbai");
+		eList = Arrays.asList(e1,e2,e3,e4,e4,e5);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		eList.clear();
+		//eList.clear();
 	}
 
 	@Test
-	public void test() {
+	public void findAllEmployeesInMumbai() {
 		List<Employee> mumbaiEmployees = eList.stream().filter(e -> e.getCity().equals("Mumbai")).collect(Collectors.toList());
-		assertTrue(mumbaiEmployees.size() == 2);
+		assertTrue(mumbaiEmployees.size() == 3);
 	}
 
+	@Test
+	public void testIfAnyEmployeesInDelhi() {
+		boolean employeesInDelhi = eList.parallelStream().anyMatch(e -> e.getCity().equals("Delhi"));
+		assertTrue(employeesInDelhi == false);
+	}
+	
+	@Test
+	public void testIfAllEmployeesHaveSalaryGreaterThanZero() {
+		boolean nonZeroSalary = eList.stream().allMatch(e -> e.getSalary() > 0);
+		assertTrue (nonZeroSalary == true);
+	}
+	
+	@Test 
+	public void testRemoveDuplicates() {
+		List<Employee> uniqueEmployees = eList.stream().distinct().collect(Collectors.toList());
+		assertTrue(uniqueEmployees.size() == 5);
+	}
+	
+	@Test
+	public void testFindEmployeesInMumbaiWithSalaryGreateThan100() {
+		List<Employee> mumbaiEmployees = eList.stream().filter(e -> e.getCity().equals("Mumbai") && e.getSalary() > 100).collect(Collectors.toList());
+		assertEquals(3, mumbaiEmployees.size());
+		//assertTrue(2,mumbaiEmployees.size());
+		
+	}
+	
+	@Test
+	public void testIncrementAllSalaryBy100() {
+		eList.stream().forEach(e -> e.setSalary(e.getSalary()+100));
+	}
+	
+	@Test
+	public void testGetFirstNamesOfAllEmployees() {
+		List<String> firstNames = eList.stream().map( e -> e.getFirstName()).collect(Collectors.toList());
+		assertTrue( firstNames.contains("Phani"));
+		
+	}
+	
+	@Test
+	public void testTotalSalary() {
+		
+	}
 }
